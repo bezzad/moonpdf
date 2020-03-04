a0e60f661b3eacda5eefb462cf7a14192d294c43
Behzad <behzad.khosravifar@gmail.com>
2020-03-04 21:39:47 +0330

    fix Out of Memory exception on viewer scrolling

--- src/MoonPdfLib/Helper/BitmapExtensionMethods.cs
+++ src/MoonPdfLib/Helper/BitmapExtensionMethods.cs
@@ -16,8 +16,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 !*/
 
 using System;
-using System.Drawing.Imaging;
-using System.Runtime.InteropServices;
 using System.Windows;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -26,44 +24,33 @@ namespace MoonPdfLib.Helper
 {
     internal static class BitmapExtensionMethods
     {
-        // [DllImport("kernel32.dll", EntryPoint = "CopyMemory", SetLastError = false)]
-        // public static extern void CopyMemory(IntPtr dest, IntPtr src, uint count);
-
         public static BitmapSource ToBitmapSource(this System.Drawing.Bitmap bmp)
         {
-            var rect = new System.Drawing.Rectangle(0, 0, bmp.Width, bmp.Height);
-            var bmpData = bmp.LockBits(rect, System.Drawing.Imaging.ImageLockMode.ReadWrite, System.Drawing.Imaging.PixelFormat.Format32bppRgb);
-            int bufferSize = bmpData.Stride * bmp.Height;
-            var bms = new System.Windows.Media.Imaging.WriteableBitmap(bmp.Width, bmp.Height, bmp.HorizontalResolution, bmp.VerticalResolution, PixelFormats.Bgr32, null);
-            bms.WritePixels(new Int32Rect(0, 0, bmp.Width, bmp.Height), bmpData.Scan0, bufferSize, bmpData.Stride);
-            bmp.UnlockBits(bmpData);
-            
-            return bms;
+            try
+            {
+                var bitmapData = bmp.LockBits(
+                    new System.Drawing.Rectangle(0, 0, bmp.Width, bmp.Height),
+                    System.Drawing.Imaging.ImageLockMode.ReadOnly, bmp.PixelFormat);
+                var bitmapSrc = BitmapSource.Create(
+                    bitmapData.Width, bitmapData.Height,
+                    bmp.HorizontalResolution, bmp.VerticalResolution,
+                    PixelFormats.Bgr24, null,
+                    bitmapData.Scan0, bitmapData.Stride * bitmapData.Height, bitmapData.Stride);
+                bmp.UnlockBits(bitmapData);
 
+                // var rect = new System.Drawing.Rectangle(0, 0, bmp.Width, bmp.Height);
+                // var bmpData = bmp.LockBits(rect, System.Drawing.Imaging.ImageLockMode.ReadWrite, System.Drawing.Imaging.PixelFormat.Format32bppRgb);
+                // var bufferSize = bmpData.Stride * bmp.Height;
+                // var bitmapSrc = new System.Windows.Media.Imaging.WriteableBitmap(bmp.Width, bmp.Height, bmp.HorizontalResolution, bmp.VerticalResolution, PixelFormats.Bgr32, null);
+                // bitmapSrc.WritePixels(new Int32Rect(0, 0, bmp.Width, bmp.Height), bmpData.Scan0, bufferSize, bmpData.Stride);
+                // bmp.UnlockBits(bmpData);
 
-
-            // var bitmapData = bmp.LockBits(
-            //     new System.Drawing.Rectangle(0, 0, bmp.Width, bmp.Height),
-            //     System.Drawing.Imaging.ImageLockMode.ReadOnly, bmp.PixelFormat);
-            //
-            // var bitmapSource = BitmapSource.Create(
-            //     bitmapData.Width, bitmapData.Height,
-            //     bmp.HorizontalResolution, bmp.VerticalResolution,
-            //     PixelFormats.Bgr24, null,
-            //     bitmapData.Scan0, bitmapData.Stride * bitmapData.Height, bitmapData.Stride);
-            //
-            // bmp.UnlockBits(bitmapData);
-            // return bitmapSource;
-
-
-            // var bms = new WriteableBitmap(bmp.Width, bmp.Height, 96.0, 96.0, PixelFormats.Bgr24, null);
-            // var data = bmp.LockBits(new System.Drawing.Rectangle(0, 0, bmp.Width, bmp.Height), ImageLockMode.ReadOnly, bmp.PixelFormat);
-            // bms.Lock();
-            // CopyMemory(bms.BackBuffer, data.Scan0, (uint)(bms.BackBufferStride * bmp.Height));
-            // bms.AddDirtyRect(new Int32Rect(0, 0, bmp.Width, bmp.Height));
-            // bms.Unlock();
-            // bmp.UnlockBits(data);
-            // return bms;
+                return bitmapSrc;
+            }
+            finally
+            {
+                GC.Collect();
+            }
         }
     }
 }
